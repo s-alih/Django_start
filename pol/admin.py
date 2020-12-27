20e6b7730a54c0341b3be8cfb686ddb3598041f5
Salih <slh32064@gmail.com>
2020-12-27 12:27:36 +0530

    index

--- pol/admin.py
+++ pol/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question,Choices
 
-admin.site.register(Question)
-admin.site.register(Choices)
+admin.site.site_header = "Pollster admin"
+admin.site.site_title = 'Pollster admin area'
+admin.site.index_title = 'Welcome to the pollster admin'
+
+# admin.site.register(Question)
+# admin.site.register(Choices)
+
+class ChoicesInline(admin.TabularInline):
+    model = Choices
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None,{'fields':['question']}),('Date Information',{'fields':['pub_date'],'classes':['collapse']}),]
+    inlines=[ChoicesInline]
+
+
+admin.site.register(Question,QuestionAdmin)
--- pol/views.py
+++ pol/views.py
@@ -1,3 +1,18 @@
+from django.http.response import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question,Choices
+
+#get questions and display them
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request,'pol/index.html',context)
+
+def detail(request,question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'pol/detail.html',{'question':question})
